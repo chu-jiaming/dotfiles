fcc9420aa2bb67f33cf1b5b047e0037f06ed73db
chu-jiaming <chujiaming143@gmail.com>
2026-07-03 18:36:59 +0800

    feat:init

---
+++ dot_config/secrets/load_api.zsh
@@ -0,0 +1,9 @@
+secrets_dir="$HOME/.config/secrets"
+
+# [[ -r "$secrets_dir/openrouter.zsh" ]] && source "$secrets_dir/openrouter.zsh"
+
+[[ -r "$secrets_dir/deepseek.zsh" ]] && source "$secrets_dir/deepseek.zsh"
+
+# [[ -r "$secrets_dir/mimo.zsh" ]] && source "$secrets_dir/mimo.zsh"
+
+unset secrets_dir
