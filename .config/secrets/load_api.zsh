71cf71b0acf9e2ec5ba22e4eae7ddbdea1deda5f
chu-jiaming <chujiaming143@gmail.com>
2026-09-19 15:39:42 +0800

    feat:rm sioyek,add vscode

--- .config/secrets/load_api.zsh
+++ .config/secrets/load_api.zsh
@@ -1,8 +1,8 @@
 secrets_dir="$HOME/.config/secrets"
 
-# [[ -r "$secrets_dir/openrouter.zsh" ]] && source "$secrets_dir/openrouter.zsh"
+[[ -r "$secrets_dir/openrouter.zsh" ]] && source "$secrets_dir/openrouter.zsh"
 
-[[ -r "$secrets_dir/deepseek.zsh" ]] && source "$secrets_dir/deepseek.zsh"
+# [[ -r "$secrets_dir/deepseek.zsh" ]] && source "$secrets_dir/deepseek.zsh"
 
 # [[ -r "$secrets_dir/mimo.zsh" ]] && source "$secrets_dir/mimo.zsh"
 
